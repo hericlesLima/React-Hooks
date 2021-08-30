d8af40b1b51ad57245872ed97cfb517772aec5b0
hericlesLima <hericlesrecheado@gmail.com>
2021-08-30 11:53:45 -0100

    useEffect

--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PageTitle from "../../components/layout/PageTitle";
 import SectionTitle from "../../components/layout/SectionTitle";
 
 const UseState = (props) => {
+  //useState
+
   const [count, setCount] = useState(0);
   const [name, setName] = useState(" ");
+  
+  //useEffect
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  }, 
+  [count]
+  );
 
   return (
     <div className="UseState">
